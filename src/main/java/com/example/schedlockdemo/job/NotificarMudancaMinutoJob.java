0427000539b152dce01c65f59c18b6d286096711
Adolfo M. Hengling <adolfo.hengling@softplan.com.br>
2021-03-12 13:54:32 -0300

    substituindo sysout por log.info

--- src/main/java/com/example/schedlockdemo/job/NotificarMudancaMinutoJob.java
+++ src/main/java/com/example/schedlockdemo/job/NotificarMudancaMinutoJob.java
@@ -1,13 +1,15 @@
 package com.example.schedlockdemo.job;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class NotificarMudancaMinutoJob {
 
     @Scheduled(cron = "0 * * * * *")
     public void run() {
-        System.out.println("[JOB] Olá, se passou mais um minuto");
+        log.info("[JOB] Olá, se passou mais um minuto");
     }
 }
